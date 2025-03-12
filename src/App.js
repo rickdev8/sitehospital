ef50cd98f30b81c007e93287986104d8ffc88b9a
RICK <rique88858@gmail.com>
2025-03-11 23:55:52 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,6 +46,7 @@ function App() {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
             entry.target.classList.add('show');
+                }
          
         });
       }, { threshold: 0.2 });
